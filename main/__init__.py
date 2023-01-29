517e70f9f10ce234d393cd65742c3412b38ed92b
amritsinghbaba01 <107133886+amritsinghbaba01@users.noreply.github.com>
2023-01-29 08:32:24 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", "17718356", cast=int)
-API_HASH = config("API_HASH", "fe2dec8e960bf8dcb4e4820d81fe7d46")
-BOT_TOKEN = config("BOT_TOKEN", "5644454947:AAHaMbdZUQgqwRrO3aZszDQ8o9AT8a2pF6E")
-SESSION = config("SESSION", "BQCf3ZJRphYVxJOGlvDIY29BPwTKR9RKYxJgJX_gNqDLZPA80z_9R1qOuA1aTbDxU7RomZXwDh4_6SC6fux7PGn_mLsVjX-m6xuV3uXPK4r4XRNVwvpv9O8RnDyMMWr8UsXX8dO5T3MgepIPa0RZCQRQ94KccoobX_VEjv2hzgeQ3t-skRwZZLTTJpa-NFN3_mrfLNcB8IA7HTMNi4nlzT5Ljbom-dtlHbQoM9Bau3ozyW16DgCYhrss3DwERrWoi022YK_0iOqpvLn0mBEwr3Eox9oinZHbLIk792XOyMX5lkh9526C1JIj-SdayRkpvtRzXNYpFoFWAMkqWTjxUHk0AAAAAVre2SwA")
-FORCESUB = config("FORCESUB", "arbackup1")
-AUTH = config("AUTH", "5819521324", cast=int)
+API_ID = config("API_ID", "22070464", cast=int)
+API_HASH = config("API_HASH", "65777c6927ee3bd024e4255a40d504e6")
+BOT_TOKEN = config("BOT_TOKEN", "5794073832:AAEHEwnF3U9bfaASYYnxkKKJgtJpApFpjoU")
+SESSION = config("SESSION", "BQBqu7r5AFsC4T0DUVQqQpSzHNo8FVPreLQfWF4ZXSLQSNppkvRYeR0QrRqiJpmvWNeheWVBKOXPpO-TAhHmDEsTsF9aBvCYub81bbRxKtk8KL6f3v1NXYa1G_lQM9ivw5rwHg1hMvyLQD2koUWAl4cz_glW0GQ67HarJOUPC_sahHEElujf8e4p_rQ_t8zDm3Nvgng8QZp48pa1qM3D1uZZ_GYx1lzor1KIXu0Q07Noj8ui3yZCiFHDZWDI6v75Kdz8DVmNkCUxnB_qkt3NgUBa88h6po_IpsJC-PegCAOJ3rWIH_q9JK8VbU-LD-KZMWVhiuD2vp_jdOpMjkJ1XzEoKUDaJQA")
+FORCESUB = config("FORCESUB", "forcesuba")
+AUTH = config("AUTH", "692116005", cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
